f80e85bf4fa1b50c53c1f6f62ee3bdb71f890cd0
Swapnil Behere <65814280+swaptheon@users.noreply.github.com>
2022-05-10 17:18:57 +0530

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -1,6 +1,4 @@
 import sys
-#Word Count Example
-# input comes from standard input STDIN
 for line in sys.stdin:
     line = line.strip() #remove leading and trailing whitespaces
     words = line.split() #split the line into words and returns as a list
